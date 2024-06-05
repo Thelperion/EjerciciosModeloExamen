ec317e042e5a263a23e2db13968c0d2dce7f79ab
Anibal <38802801+Thelperion@users.noreply.github.com>
2024-06-05 13:57:03 +0200

    pom.xml

---
+++ src/main/java/MODELOEXAMEN/modeloExamen/ModeloExamenApplication.java
@@ -0,0 +1,13 @@
+package MODELOEXAMEN.modeloExamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeloExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeloExamenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MODELOEXAMEN/modeloExamen/ModeloExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package MODELOEXAMEN.modeloExamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeloExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
